b48bd374336fbbd9e9f984d6e0fef0cd130a75d8
visal1234 <bzhou@paragoniu.edu.kh>
2023-07-30 00:11:55 +0700

    finish layout

--- wensday-club-main/app/layout.tsx
+++ wensday-club-main/app/layout.tsx
@@ -1,8 +1,14 @@
+'use client'
+
+import Image from "next/image"
+import { useState } from "react"
+
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
+
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -14,11 +20,36 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  const [active, setActive] = useState(1)
   return (
     <html lang="en">
       <body className="flex-center">
-				<main className="w-[1500px] bg-blue-500">{children}</main>
-    	</body>
+        <main className="w-[1500px]">
+          <div className="flex">
+            <div className="w-[310px] h-screen fixed bg-gradient-to-b from-sky-300 to-rose-300">
+              <div className="flex-center mt-20 mb-32">
+                <Image src="/Olivier_1500_Trptch.jpg" width={220} height={200} alt="profile picture" className="rounded-[50%] aspect-square object-cover " />
+              </div>
+              <button onClick={() => setActive(1)} className={"block mx-auto w-48 py-2 my-10 text-xl font-bold " + (active === 1 ? "bg-debian-red text-white bg-opacity-75 hover:bg-opacity-100" : "text-debian-red hover:text-white hover:bg-debian-red")}>Home</button>
+              <button onClick={() => setActive(2)} className={"block mx-auto w-48 py-2 my-10 text-xl font-bold " + (active === 2 ? "bg-debian-red text-white bg-opacity-75 hover:bg-opacity-100" : "text-debian-red hover:text-white hover:bg-debian-red")}>Chat</button>
+              <button onClick={() => setActive(3)} className={"block mx-auto w-48 py-2 my-10 text-xl font-bold " + (active === 3 ? "bg-debian-red text-white bg-opacity-75 hover:bg-opacity-100" : "text-debian-red hover:text-white hover:bg-debian-red")}>Saved</button>
+              <button className="block px-3 py-2 mx-auto mt-80 w-36 bg-debian-red text-white rounded-[20px] text-xl font-bold">Log Out</button>
+            </div>
+            {/* the sidebar width below the real fixed sidebar */}
+            <div className="w-[390px]"></div>
+            <div className="w-full">
+              <div className="flex items-center w-full h-[80px] bg-debian-red">
+                <button className="flex mx-20 my-5 text-white text-xl font-bold bg-purple-800 px-5 py-2 shadow-sm shadow-black rounded-[30px]">Wensday Club</button>
+                <div className="ml-auto w-60 mr-10 relative leading-[2.25]">
+                  <input className="block pr-2 pl-3 shadow-sm shadow-black w-4/5 rounded-[15px] box-content" type="text" placeholder="Search" />
+                  <img className="absolute right-[33px] top-[-8px] scale-[70%] opacity-60 border-l-[3px] border-gray-400 pl-2 py-[0.625rem] " src="/search.png" alt="" />
+                </div>
+              </div>
+            {children}
+            </div>
+          </div>
+        </main>
+      </body>
     </html>
   )
 }
--- wensday-club-main/app/page.tsx
+++ wensday-club-main/app/page.tsx
@@ -1,29 +1,8 @@
-'use client'
-
-import Image from "next/image"
-import { useState } from "react"
-
-
 export default function Home() {
-  const [active, setActive] = useState(1)
+  
   return (
     <>
-      <div className="flex">
-        <div className="w-[310px] h-screen fixed bg-gradient-to-b from-sky-300 to-rose-300">
-          <div className="flex-center mt-20 mb-32">
-            <Image src="/Olivier_1500_Trptch.jpg" width={220} height={200} alt="profile picture" className="rounded-[50%] aspect-square object-cover " />
-          </div>
-          <button onClick={() => setActive(1)} className={"block mx-auto w-48 py-2 my-10 text-xl font-bold " + (active === 1 ? "bg-debian-red text-white bg-opacity-75 hover:bg-opacity-100" : "text-debian-red hover:text-white hover:bg-debian-red")}>Home</button>
-          <button onClick={() => setActive(2)} className={"block mx-auto w-48 py-2 my-10 text-xl font-bold " + (active === 2 ? "bg-debian-red text-white bg-opacity-75 hover:bg-opacity-100" : "text-debian-red hover:text-white hover:bg-debian-red")}>Chat</button>
-          <button onClick={() => setActive(3)} className={"block mx-auto w-48 py-2 my-10 text-xl font-bold " + (active === 3 ? "bg-debian-red text-white bg-opacity-75 hover:bg-opacity-100" : "text-debian-red hover:text-white hover:bg-debian-red")}>Saved</button>
-          <button className="block px-3 py-2 mx-auto mt-80 w-36 bg-debian-red text-white rounded-[20px] text-xl font-bold">Log Out</button>
-        </div>
-        {/* the sidebar width below the real fixed sidebar */}
-        <div className="w-[390px]"></div>
-        <div className="w-full h-[80px] bg-debian-red">
-            <button className="flex mx-20 my-5 text-white text-xl font-bold bg-blue-600 px-5 py-2">Wenzday Club</button>
-        </div>
-      </div>
+      Hello There  
     </>
   )
 }
