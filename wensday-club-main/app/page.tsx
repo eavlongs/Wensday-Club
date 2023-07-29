c606142ee56a21479527e72da72d509817fc2418
PrakBunlong <106743681+PrakBunlong@users.noreply.github.com>
2023-07-29 22:24:16 +0700

    Update page.tsx

--- wensday-club-main/app/page.tsx
+++ wensday-club-main/app/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image"
 import { useState } from "react"
-import Button from "../components/Button"
+
 
 export default function Home() {
     const [active, setActive] = useState(1)
