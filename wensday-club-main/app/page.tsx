c9cee5de2652bdde009f5fd4d36306ec50504e7b
eavlongsok <esok@paragoniu.edu.kh>
2023-08-01 20:30:53 +0700

    made the layout "responsive" :)

--- wensday-club-main/app/page.tsx
+++ wensday-club-main/app/page.tsx
@@ -1,8 +1,111 @@
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: 'Wensday Club',
+    description: 'Join Wensday Club today',
+    viewport: "width=device-width, initial-scale=1",
+}
+
 export default function Home() {
-  
+
   return (
     <>
-      Hello There  
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
+      <p>Hello There</p>
     </>
   )
 }
